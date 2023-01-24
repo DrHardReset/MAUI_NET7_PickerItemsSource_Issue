b4a520ab55e23e8a1f6482710dfc1f7fc6ffef93
DrHardReset <brillebralle@web.de>
2023-01-24 20:23:00 +0100

    initial commit

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+    List<string> list1 = new List<string>() { "1" };
+    List<string> list2 = new List<string>() { "2" };
+
+    public MainPage()
+    {
+        InitializeComponent();
+        Picker1.ItemsSource = list1;
+        Picker1.SelectedIndex = 0;
+    }
+
+    private void OnButtonClicked(object sender, EventArgs e)
+    {
+        Picker1.ItemsSource = list2;
+        Picker1.SelectedIndex = 0;
+    }
+}
+
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<string> list1 = new List<string>() { "1" };
+        List<string> list2 = new List<string>() { "2" };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ComboBox1.ItemsSource = list1;
+            ComboBox1.SelectedIndex = 0;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ComboBox1.ItemsSource = list2;
+            ComboBox1.SelectedIndex = 0;
+        }
+    }
+}
